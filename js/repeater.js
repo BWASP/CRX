b94294225eafe4c7ddb91d14db6dff1a264c2e2b
dohunny <dhkoo.hacker@gmail.com>
2021-12-14 11:48:05 +0900

    repeater 코드 클래스화

--- js/repeater.js
+++ js/repeater.js
@@ -1,54 +1,52 @@
-let init_option = new Object();
 
-function init_repeater(){
-    init_option = {
-        "background_port": chrome.runtime.connect({name: "repeater"}),
-        "packet_list": Array(),
-        "packet_idx": 0,
-        "packet_count": 0,
-        "request_area": document.getElementById("requestTextarea"),
-        "response_area": document.getElementById("responseTextarea"),
+class Repeater {
+    constructor() {
+        this.init_option = {
+            "background_port": chrome.runtime.connect({name: "repeater"}),
+            "packet_list": Array(),
+            "packet_idx": 0,
+            "packet_count": 0,
+            "request_area": document.getElementById("requestTextarea"),
+            "response_area": document.getElementById("responseTextarea"),
+            "send_button": document.getElementById("repeater-send"),
+        }
+    }
+
+    header_parse(packet) {
+        let headerSet = Object.keys(packet["headers"]);
+        let data = "";
+        headerSet.forEach((header) => {
+            data += header + ": " + JSON.stringify(packet["headers"][header]) + "\r\n";
+        });
+        return data;
     }
     
-    // init_option['request_area'].innerText = ""
-    // init_option['response_area'].innerText = ""
-};
-
-function header_parse(packet) {
-    let headerSet = Object.keys(packet["headers"]);
-    let data = "";
-    headerSet.forEach((header) => {
-        data += header + ": " + JSON.stringify(packet["headers"][header]) + "\r\n";
-    });
-    return data;
+    packet_parse(mode, packet) {
+        let data = "";
+    
+        switch (mode) {
+            case "req":
+                data += packet['method'] + " " + packet['url'] + " " + "HTTP/1.1" + "\r\n";
+                data += "Host: " + packet["full_url"] + "\r\n";
+                data += this.header_parse(packet);
+                data += "\r\n" + packet["body"];
+                break
+    
+                case "res":
+                console.log("response status: ", packet);
+                data += packet['protocol'] + " " + packet["status"] + " " + packet["statusText"] + "\r\n"
+                data += this.header_parse(packet);
+                data += "\r\n" + packet["body"];
+                break
+            }
+    
+            return data;
+        }
 }
 
-function packet_parse(mode, packet) {
-    let data = "";
+let repeater = new Repeater();
 
-    switch (mode) {
-        case "req":
-            data += packet['method'] + " " + packet['url'] + " " + "HTTP/1.1" + "\r\n";
-            data += "Host: " + packet["full_url"] + "\r\n";
-            data += header_parse(packet);
-            data += "\r\n" + packet["body"];
-            break
-
-        case "res":
-            console.log("response status: ", packet);
-            data += packet['protocol'] + " " + packet["status"] + " " + packet["statusText"] + "\r\n"
-            data += header_parse(packet);
-            data += "\r\n" + packet["body"];
-            break
-    }
-
-    return data;
-}
-
-
-init_repeater();
-
-init_option['background_port'].onMessage.addListener(function (msg) {
+repeater.init_option['background_port'].onMessage.addListener(function (msg) {
     if (msg.type == "RequestPackets") {
         console.log("Received Request Packets Successfully!")
         
@@ -62,8 +60,8 @@ init_option['background_port'].onMessage.addListener(function (msg) {
             });
         });
 
-        init_option['packet_list'] = init_option['packet_list'].concat(packetList);
-        init_option['request_area'].value = packet_parse("req",init_option['packet_list'][init_option['packet_idx']]['request'])
-        init_option['response_area'].value = packet_parse("res",init_option['packet_list'][init_option['packet_idx']]['response'])
+        repeater.init_option['packet_list'] = repeater.init_option['packet_list'].concat(packetList);
+        repeater.init_option['request_area'].value = repeater.packet_parse("req",repeater.init_option['packet_list'][repeater.init_option['packet_idx']]['request'])
+        repeater.init_option['response_area'].value = repeater.packet_parse("res",repeater.init_option['packet_list'][repeater.init_option['packet_idx']]['response'])
     }
 });

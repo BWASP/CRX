7ae5ef54ac430afa19e6b9eefd4b181c0309dc9e
dohunny <dhkoo.hacker@gmail.com>
2021-12-30 14:44:51 +0900

    repeater 패킷 파싱&전송 후 응답 body 가져오는 기능 구현

--- js/repeater.js
+++ js/repeater.js
@@ -9,6 +9,7 @@ class Repeater {
             "request_area": document.getElementById("requestTextarea"),
             "response_area": document.getElementById("responseTextarea"),
             "send_button": document.getElementById("repeater-send"),
+            "xhr": new XMLHttpRequest(),
         }
     }
 
@@ -16,12 +17,13 @@ class Repeater {
         let headerSet = Object.keys(packet["headers"]);
         let data = "";
         headerSet.forEach((header) => {
-            data += header + ": " + JSON.stringify(packet["headers"][header]) + "\r\n";
+            // console.log('headers: ', packet['headers'][header]);
+            data += header + ": " + packet["headers"][header].toString() + "\r\n";
         });
         return data;
     }
     
-    packet_parse(mode, packet) {
+    packet_stringify(mode, packet) {
         let data = "";
     
         switch (mode) {
@@ -32,23 +34,95 @@ class Repeater {
                 data += "\r\n" + packet["body"];
                 break
     
-                case "res":
-                console.log("response status: ", packet);
+            case "res":
+                // console.log("response status: ", packet);
                 data += packet['protocol'] + " " + packet["status_code"] + " " + packet["status_text"] + "\r\n"
                 data += this.header_parse(packet);
                 data += "\r\n" + packet["body"];
                 break
-            }
-    
-            return data;
         }
+    
+        return data;
+    }
+
+    packet_parse(packet) {
+        let host_url = "https://webhacking.kr/";
+
+        // preprocessing(전처리과정) => 패킷을 라인별로 배열에 저장
+        packet = packet.replace('\r\n','\n');
+        let body = packet.split('\n\n')[1];
+        packet = packet.split('\n\n')[0];
+        let packet_per_line = packet.split('\n');
+        // console.log("packet per lines: ", packet_per_line);
+        
+        // first line: Method Path Protocol/Version
+        let first_line = packet_per_line[0].split(' ');
+        packet_per_line.shift();
+        // console.log("first_line: ", first_line);
+        
+        // 헤더 정보 파싱 후 JSON 형태로 변환
+        let headers = new Object();
+        packet_per_line.forEach((header_line) => {
+            if (header_line == "") return false;
+            headers[header_line.split(": ")[0]] = header_line.split(": ")[1];
+            packet_per_line.shift();
+        });
+        // console.log("headers: ", headers);
+
+        // 바디 정보 가져오기
+        try {
+            body = JSON.stringify(JSON.parse(body))
+        } catch {
+            let form_data = new Object(); 
+            body.split('&').forEach((param) => {
+                if (param == "") return false;
+                form_data[param.split('=')[0]] = param.split('=')[1];
+            });
+            body = JSON.stringify(form_data);
+        }
+        // console.log("body: ", body);
+
+        this.send_request_packet(first_line[0], new URL(first_line[1], host_url).href, headers, body);
+    }
+
+    send_request_packet(method, url, headers, body){
+        this.init_option['xhr'].open(method, url);
+
+        // XMLHTTPRequest header 설정
+        let headers_key = Object.keys(headers)
+        headers_key.forEach((key) => {
+            try {
+                this.init_option['xhr'].setRequestHeader(key, headers[key]);
+            } catch {
+                
+            }
+            // console.log('xhr headers: ', key, headers[key]);
+        });
+
+        this.init_option['xhr'].onreadystatechange = function (event) { 
+            let { target } = event; 
+            if (target.readyState === XMLHttpRequest.DONE) { 
+                let { status } = target; 
+                if (status === 0 || (status >= 200 && status < 400)) { 
+                    // 요청이 정상적으로 처리 된 경우 
+                    // console.log("요청이 정상적으로 처리된 경우");
+                    console.log("response: ", target.response)
+                } else { 
+                    // 에러가 발생한 경우 
+                    // console.log("에러가 발생한 경우");
+                } 
+            } 
+        };
+        this.init_option['xhr'].send(body);
+        // console.log("Sent Request Packet Successfully!!!");
+    }
 }
 
 let repeater = new Repeater();
 
 repeater.init_option['background_port'].onMessage.addListener(function (msg) {
     if (msg.type == "RequestPackets") {
-        console.log("Received Request Packets Successfully!")
+        // console.log("Received Request Packets Successfully!")
         
         let packetList = Array();
         let dataSet = msg.data;
@@ -61,7 +135,11 @@ repeater.init_option['background_port'].onMessage.addListener(function (msg) {
         });
 
         repeater.init_option['packet_list'] = repeater.init_option['packet_list'].concat(packetList);
-        repeater.init_option['request_area'].value = repeater.packet_parse("req",repeater.init_option['packet_list'][repeater.init_option['packet_idx']]['request'])
-        repeater.init_option['response_area'].value = repeater.packet_parse("res",repeater.init_option['packet_list'][repeater.init_option['packet_idx']]['response'])
+        repeater.init_option['request_area'].value = repeater.packet_stringify("req",repeater.init_option['packet_list'][repeater.init_option['packet_idx']]['request'])
+        repeater.init_option['response_area'].value = repeater.packet_stringify("res",repeater.init_option['packet_list'][repeater.init_option['packet_idx']]['response'])
     }
+});
+
+repeater.init_option['send_button'].addEventListener('click', function() {
+    repeater.packet_parse(repeater.init_option['request_area'].value);
 });
